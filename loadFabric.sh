07f94131d1c772bcc07aff98a5334ff97724e272
Juan M. Sanchez <juansaga00@gmail.com>
2022-12-12 10:04:09 -0500

    3 nodes network

---
+++ loadFabric.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# load fabric images 2.1.0 fabric-ca 1.4.7 fabic-couchdb 0.4.20
+curl -sSL https://raw.githubusercontent.com/hyperledger/fabric/master/scripts/bootstrap.sh | bash -s -- 2.1.0 1.4.7 0.4.20
+echo "========= download completed =========== "
+cp -R fabric-samples/bin .
+cp -R fabric-samples/config .
+echo "========= completed config set up =========== "
